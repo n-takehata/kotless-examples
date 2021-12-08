a006619ffcb999955eb883af2782c7a4bde6e2a6
竹端尚人 <take7010@gmail.com>
2021-12-09 08:58:59 +0900

    added logic for get items

--- kotless-and-dynamodb/src/main/kotlin/com/example/kotless/DynamoDb.kt
+++ kotless-and-dynamodb/src/main/kotlin/com/example/kotless/DynamoDb.kt
@@ -1,5 +1,15 @@
 package com.example.kotless
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
+import com.amazonaws.services.dynamodbv2.model.AttributeValue
+import com.amazonaws.services.dynamodbv2.model.GetItemRequest
+
 fun findItem(): String {
-    return "find item"
+    val client = AmazonDynamoDBClientBuilder.defaultClient()
+    val request = GetItemRequest()
+        .withKey(mapOf("id" to AttributeValue().withN("101")))
+        .withTableName("Commodity")
+    val result = client.getItem(request).item
+
+    return result["id"].toString()
 }
