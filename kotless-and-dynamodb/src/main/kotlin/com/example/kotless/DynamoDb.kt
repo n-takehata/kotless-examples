2c2e89adf6f1c23de83f1312a18b6e4e648e38dc
竹端尚人 <take7010@gmail.com>
2021-12-10 09:14:25 +0900

    fix logic for create DynamoDB client

--- kotless-and-dynamodb/src/main/kotlin/com/example/kotless/DynamoDb.kt
+++ kotless-and-dynamodb/src/main/kotlin/com/example/kotless/DynamoDb.kt
@@ -3,9 +3,11 @@ package com.example.kotless
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
 import com.amazonaws.services.dynamodbv2.model.AttributeValue
 import com.amazonaws.services.dynamodbv2.model.GetItemRequest
+import io.kotless.AwsResource
+import io.kotless.dsl.lang.withKotlessLocal
 
 fun findItem(): String {
-    val client = AmazonDynamoDBClientBuilder.defaultClient()
+    val client = AmazonDynamoDBClientBuilder.standard().withKotlessLocal(AwsResource.DynamoDB).build()
     val request = GetItemRequest()
         .withKey(mapOf("id" to AttributeValue().withN("101")))
         .withTableName("Commodity")
