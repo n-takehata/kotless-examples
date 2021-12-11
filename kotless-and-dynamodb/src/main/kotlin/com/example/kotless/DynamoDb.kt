a147e51e51c1483b461568b65e3844b8487a166c
竹端尚人 <take7010@gmail.com>
2021-12-11 11:52:06 +0900

    added scheduled function

--- kotless-and-dynamodb/src/main/kotlin/com/example/kotless/DynamoDb.kt
+++ kotless-and-dynamodb/src/main/kotlin/com/example/kotless/DynamoDb.kt
@@ -4,9 +4,7 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
 import com.amazonaws.services.dynamodbv2.model.AttributeValue
 import com.amazonaws.services.dynamodbv2.model.GetItemRequest
 import com.amazonaws.services.dynamodbv2.model.PutItemRequest
-import com.amazonaws.services.dynamodbv2.model.PutItemResult
-import io.kotless.AwsResource
-import io.kotless.dsl.lang.withKotlessLocal
+import io.kotless.dsl.lang.event.Scheduled
 
 fun putItem(): String {
     val id = (Math.random() * 1000).toInt().toString()
@@ -30,4 +28,18 @@ fun findItem(): String {
     val result = client.getItem(request).item
 
     return result["id"].toString()
+}
+
+@Scheduled(Scheduled.everyMinute)
+private fun putItemEveryMinute() {
+    val id = (Math.random() * 1000).toInt().toString()
+    val client = AmazonDynamoDBClientBuilder.defaultClient()
+    val items = mapOf(
+        "id" to AttributeValue().withN(id),
+        "name" to AttributeValue().withS("Tomato"),
+        "price" to AttributeValue().withN("1000")
+    )
+    val request = PutItemRequest().withItem(items).withTableName("Commodity")
+
+    client.putItem(request)
 }
